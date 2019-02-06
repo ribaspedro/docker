d6f0c865c76c4311bc12e7cdf979c26397af69e4
ribaspedro <pedro.ferreira@geopx.com.br>
2019-02-06 17:08:19 -0200

    First attempt to download geopixel monitor

--- terrama2/files/install_terrama2.sh
+++ terrama2/files/install_terrama2.sh
@@ -14,7 +14,7 @@ apt-get update
 
 # TODO: Change URL to http://www.dpi.inpe.br/terrama2/doku.php?id=download and change curl parameter to -o deb_package_name
 echo -ne "Downloading TerraMA² ... "
-curl -O http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/TerraMA2-${TERRAMA2_TAG}-release-linux-x64-Ubuntu-16.04.deb --silent
+curl -O http://l1.geopx.com.br:8080/ImageLayer/TerraMA2-${TERRAMA2_TAG}-release-linux-x64-Ubuntu-16.04.deb --silent
 valid $? "Error: Could not fetch TerraMA²"
 
 echo -ne "Downloading TerraMA² documentation ..."
